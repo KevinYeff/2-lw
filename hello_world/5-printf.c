64b6e2d490975e1364df3cdb7c5d6b9c83ef878e
KevinYeff <kevinyeff94@gmail.com>
2022-06-06 14:13:56 -0700

    Task 5 Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * void is a good practice.
+ * printf() function prints a string on the screen using a “format string”
+ * that includes the instructions to mix several strings
+ * and produce the final string to be printed on the screen.
+*/
+
+#include <stdio.h>
+
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art,.
+ * Return: (0).
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
