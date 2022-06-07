6889e0294654c3fefdb01261b63f47b395843544
KevinYeff <kevinyeff94@gmail.com>
2022-06-06 19:23:46 -0700

    Task 8 UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19".
+ * Return: (1).
+ */
+int main(void)
+{
+fwrite('and that piece of art is useful" - Dora Korpar, 2015-10-19\n', 59, 2);
+return (1);
+}
+
