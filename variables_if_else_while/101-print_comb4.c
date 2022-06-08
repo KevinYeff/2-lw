9ffdb02ec6dd27a5056b8d239bca53d33a14a208
KevinYeff <kevinyeff94@gmail.com>
2022-06-08 14:41:34 -0700

    Task 11

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible different combinations of three digits.
+ * Return: Shoulc be 0.
+ */
+int main(void)
+{
+	int n1, n2, n3;
+
+	for (n1 = '0'; n1 <= '9'; n1++)
+	{
+		for (n2 = '0'; n2 <= '9'; n2++)
+		{
+			for (n3 = '0'; n3 <= '9'; n3++)
+			{
+				if ((n1 < n2) && n2 < n3)
+				{
+					putchar(n1);
+					putchar(n2);
+					putchar(n3);
+					if (n1 != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
