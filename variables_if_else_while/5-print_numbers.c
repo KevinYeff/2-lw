a4bcb160908c249b8de2d11b50543fb58ecc1bf3
KevinYeff <kevinyeff94@gmail.com>
2022-06-08 08:10:22 -0700

    Task 5 Numbers fixed

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
  * main - Prints all single digit numbers of base 10 starting from 0,
  *        followed by a ney line.
- * Resturn: Should be 0.
+ * Return: Should be 0.
  */
 
 int main(void)
