803f8854b41ae37ace5a535ba8876ad2fe545db7
KevinYeff <kevinyeff94@gmail.com>
2022-06-07 22:54:18 -0700

    Task 2 alphabet game

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lower case followed
+ *        by a new line.
+ * Return: should be 0.
+ */
+int main(void)
+{
+	char lc; /* Declaring Data Type "char" as lc */
+	for(lc = 'a'; lc <= 'z'; lc++)
+		putchar(lc);
+	putchar('\n');
+    return 0;
+}
