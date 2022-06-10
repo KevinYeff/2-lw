b43b022581171887cb76bda55b5fd0f7918c5196
KevinYeff <kevinyeff94@gmail.com>
2022-06-09 18:46:42 -0700

    Task 2 and 3rd main.h

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lower
+ *                      case followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	char al;
+	int c = 1;
+
+	while (c < 10)
+	{
+		al = 97;
+		while (al <= 122)
+		{
+			_putchar(al);
+			al++;
+		}
+		_putchar(10);
+		c++;
+	}
+}
