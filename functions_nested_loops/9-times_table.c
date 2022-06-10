d806cecc76f4742011cdaa6a95806582a1ce70c2
KevinYeff <kevinyeff94@gmail.com>
2022-06-10 08:38:14 -0700

    Task 9 fixed

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,23 +9,23 @@ void times_table(void)
 {
 	int n, f, r;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n < 10; n++)
 	{
-		for (f = 0; f <= 9; f++)
+		for (f = 0; f < 10; f++)
 		{
 			r = n * f;
 			if (f == 0)
 			{
 				_putchar(r + '0');
 			}
-			else if (r >= 9)
+			else if (r >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar((r / 10) + '0');
 				_putchar((r % 10) + '0');
 			}
-			else if (r <= 9)
+			else if (r < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
