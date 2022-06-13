72acb6354d1e27aa2cb07e525b4721d98e699192
KevinYeff <kevinyeff94@gmail.com>
2022-06-13 16:24:29 -0700

    Task 0 betty friendly

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,10 @@
 #include "main.h"
+
 /**
  * _isupper - Function that checks for an uppercase character.
+ *
+ * @c: Is the integer to check.
+ *
  * Return: 1 if character us uppercase otherwise returns 0.
  */
 int _isupper(int c)
