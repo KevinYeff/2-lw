457c5a1bfbd3a682be2384995f86ee36c876807b
KevinYeff <kevinyeff94@gmail.com>
2022-06-13 16:57:50 -0700

    Task2 multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiplies tow integers.
+ *
+ * @a: Integer number 1.
+ * @b: Integer number 2.
+ *
+ * Return: a * b.
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
