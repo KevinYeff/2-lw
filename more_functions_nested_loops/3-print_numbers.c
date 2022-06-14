06ed33316a6f793cf788b5445470e826c69bea45
KevinYeff <kevinyeff94@gmail.com>
2022-06-13 17:22:45 -0700

    Task 3 numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - function that prints the numbers from 0 to 9.
+ *
+ * @h: the int to be displayed.
+ *
+ * Return: Should be 0.
+ */
+
+void print_numbers(void)
+{
+	int h = 0;
+	for (h = 0; h <= 9; h++)
+	{
+		_putchar(h + '0');
+	}
+	_putchar(10);
+}
