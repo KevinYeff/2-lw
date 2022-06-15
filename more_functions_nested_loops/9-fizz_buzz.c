609ea00bc8880936f6285331844e45d24c59fc6d
KevinYeff <kevinyeff94@gmail.com>
2022-06-14 17:19:02 -0700

    Taks 9 spaces fixed

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,25 +16,28 @@ int main(void)
 
 	for (h = 1; h <= 100; h++)
 	{
+		if (h < 100)
+		{
+			printf(" ");
+		}
 		if (h % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 
 		else if (h % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (h % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
 			printf("%d", h);
-			printf(" ");
 		}
 	}
-	printf("\n");
+	printf(" ");
 	return (0);
 }
