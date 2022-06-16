7d7977cbc1d9e68bba423ff2079fc2dec266b4d0
root <root@DESKTOP-0PHR9OP.localdomain>
2022-06-16 11:01:18 -0500

    Task 4 string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - Fuctions that prints a string in reverse,
+ *             followed by a new line.
+ *
+ *@s: String that shold be printed in reverse.
+ */
+
+void print_rev(char *s)
+{
+	int h, k;
+
+	k = strlen(s);
+/* strlen() calculates the length of the string pointed to*/
+	for (h = k - 1; h >= 0; h--)
+	{
+		_putchar(s[h]);
+	}
+}
