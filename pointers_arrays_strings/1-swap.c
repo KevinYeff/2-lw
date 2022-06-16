b84026e2e4b8dcce94a742298f36571c50248f84
root <root@DESKTOP-0PHR9OP.localdomain>
2022-06-16 01:57:15 -0500

    Task 1 swapping

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Function that swaps the values of two integers.
+ *
+ * @a: First pointer.
+ * @b: Second pointer.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
