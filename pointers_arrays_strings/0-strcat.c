b5c7bd5f26e62cd6b07cefe758d8d72870cab308
root <root@DESKTOP-0PHR9OP.localdomain>
2022-06-17 01:52:36 -0500

    Task 0 strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - Concatenates two strings.
+ *
+ * @dest: Destination.
+ * @src: Source.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *s3 = strcat(dest, src);
+
+	return (s3);
+}
