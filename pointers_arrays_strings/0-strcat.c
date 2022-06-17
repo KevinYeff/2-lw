ec0b8a4b4dc79e99af2c46a59727be451406e53d
root <root@DESKTOP-0PHR9OP.localdomain>
2022-06-17 02:20:40 -0500

    Task 0 strcat betty friendly

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * _strcat - Concatenates two strings.
  *
  * @dest: Destination.
  * @src: Source.
+ * Return: Pointer to the resulting string.
  */
 
 char *_strcat(char *dest, char *src)
