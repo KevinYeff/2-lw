a87de703a79b7ea5fb3970b1c10421cf88c5f7e9
root <root@DESKTOP-0PHR9OP.localdomain>
2022-06-19 18:53:16 -0500

    Task 0 memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memset - Fills n bytes of memory area pointed to by s with
+ *           the constant byte b.
+ *
+ * @s: Pointer.
+ * @b: Char variable.
+ * @n: Unsigned char. N bytes to be filled.
+ *
+ * Return: The pointer to the memory area s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int h;
+
+	for (h = 0; h < n; h++)
+	{
+		s[h] = b;
+	}
+	return(s);
+}
