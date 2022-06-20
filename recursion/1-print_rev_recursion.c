d6308bfe1ab74ab0e16883889e3acd3d96ca3c88
root <root@DESKTOP-0PHR9OP.localdomain>
2022-06-20 18:01:29 -0500

    Task 1 String in rev

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a strinjg in reverse using
+ *                        recursion.
+ *
+ * @s: String.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int h;
+
+	h = 0;
+
+	if (s[h] != '\0')
+	{
+		_print_rev_recursion(&(s[h + 1]));
+		 _putchar(s[h]);
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
