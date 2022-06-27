9e9558f99310ad18b9e904f661f85f268b4570e6
root <root@DESKTOP-0PHR9OP.localdomain>
2022-06-26 23:15:01 -0500

    Task 1 fix atmp

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -18,25 +18,25 @@ char *_strdup(char *str)
 	int h, k;
 
 	if (str == NULL)
-	{
+
 		return (NULL);
-	}
+
 
 	for (h = 0; str[h] != '\0'; h++)
-	{
+
 		k++;
-	}
+
 	k += 1;
 
 	s = malloc(k * sizeof(char));
 
 	if (s == NULL)
-	{
+
 		return (NULL);
-	}
+
 	for (h = 0; h < str[h]; h++)
-	{
+
 		s[h] = str[h];
-	}
+
 	return (s);
 }
