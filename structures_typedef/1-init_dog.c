307e7686fed787d8e2a1b55574d4d69cb11b4322
root <root@DESKTOP-0PHR9OP.localdomain>
2022-07-01 01:15:07 -0500

    Task 1 initilizing a struct

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Function that initialize a variable of type struct dog
+ *
+ * @d: Pointer to elements of my structure.
+ * @name: Name of some dog.
+ * @age: Age ofthe dog.
+ * @owner: Dog owner name.
+ *
+ * Return: void.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
