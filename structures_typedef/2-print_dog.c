71b05e1e1fe003fed7f64361a7b449f59eaf0e08
root <root@DESKTOP-0PHR9OP.localdomain>
2022-07-01 02:02:52 -0500

    Task 2 float fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,10 +17,13 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			printf("Name: (nill)\n");
 		else
-		{
 			printf("Name: %s\n", d->name);
+
+		if (d->age < 0)
+			printf("Age: (nill)\n");
+		else
 			printf("Age : %f\n", d->age);
-		}
+
 		if (d->owner == NULL)
 			printf("Owner: (nill)\n");
 		else
