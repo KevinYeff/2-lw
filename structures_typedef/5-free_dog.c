c56d2ba074d433df7b22b79fe17ee8dc276c41a2
root <root@DESKTOP-0PHR9OP.localdomain>
2022-07-02 23:02:13 -0500

    Task 5 Functions that frees a struct

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - Function that frees the struct.
+ *
+ * @d: The struct.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+	else
+		return;
+}
