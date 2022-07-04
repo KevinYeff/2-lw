9ac64198bd28441bdacd223ac600ef9422253956
root <root@DESKTOP-0PHR9OP.localdomain>
2022-07-03 19:49:02 -0500

    Task 3 while loop

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,10 +22,11 @@ int (*get_op_func(char *s))(int a, int b)
 	};
 	int i;
 
-	for (i = 0; ops[i].op; i++)
+	while (ops[i].op)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
+		i++;
 	}
 
 	printf("Error\n");
