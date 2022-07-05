9d3e3d14bc5a602ab485191a1bbd4c68c7d168de
root <root@DESKTOP-0PHR9OP.localdomain>
2022-07-05 13:01:40 -0500

    Task 2 prints string

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - Function that prints strings, followed by anew line.
+ *
+ * @separator: Separator between strings.
+ * @n: Number of strings passed to the function.
+ * @...: Strings to be printed.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *psrt;
+	unsigned int i;
+	va_list nstrings;
+
+	va_start(nstrings, n);
+	for (i = 0; i < n; i++)
+	{
+		psrt = va_arg(nstrings, char *);
+
+		if (nstrings == NULL)
+			printf("(nill)");
+		else
+			printf("%s", psrt);
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(nstrings);
+	printf("\n");
+}
